9c612600944d92645e1369666923e62c960ddc21
JelouaneAlae03 <jelouanealae.job@gmail.com>
2024-12-07 23:38:31 +0100

    tp-4 && tp-5

---
+++ my-react-app/src/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+// import Home from "./Home";
+import Products from "./Product";
+// import ProductDetails from "./ProductDetails";
+
+
+function Main() {
+    return (
+        <Products></Products>
+    );
+}
+export default Main;
